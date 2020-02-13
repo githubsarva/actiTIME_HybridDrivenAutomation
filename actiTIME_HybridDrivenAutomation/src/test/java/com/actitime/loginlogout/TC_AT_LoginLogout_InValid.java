22735073df5783047a801e4f4bf3677874b39fca
githubsarva <mailtosaravananl1991@gmail.com>
2020-02-13 07:13:34 +0530

    Implement pom and commiting

--- actiTIME_HybridDrivenAutomation/src/test/java/com/actitime/loginlogout/TC_AT_LoginLogout_InValid.java
+++ actiTIME_HybridDrivenAutomation/src/test/java/com/actitime/loginlogout/TC_AT_LoginLogout_InValid.java
@@ -6,18 +6,29 @@ import org.testng.annotations.Test;
 import com.actitime.pageObjects.AT_LoginLogout_Valid;
 import com.actitime.utilities.Config;
 
+import junit.framework.Assert;
+
 public class TC_AT_LoginLogout_InValid extends Config{
 	@Test
 	public void TC_AT_LoginLogout_InValid_Test() throws InterruptedException{
 		ps.navigatetestURL();
 		lo = new AT_LoginLogout_Valid(driver);
-			lo.setusername("add");
-		lo.setPassword("lll");
+			lo.setusername("admin11");
+		lo.setPassword("manager11");
 		lo.login();
-		
+		/*
 		String s= driver.findElement(By.className("errormsg")).getText();
 		System.out.println(s);
-		ps.verifyErrorMsg("Username or Password is invalid. Please try again.");
+		ps.verifyErrorMsg("Username or Password is invalid. Please try again.");*/
+		
+		Thread.sleep(3000);
+		
+		if(driver.getCurrentUrl().equals("http://localhost/login.do")){
+			Assert.assertTrue(true);
+		}else{
+			Assert.assertTrue(false);
+		}
+		
 		
 	}
 
